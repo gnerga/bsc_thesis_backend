6a7882a3f4a705b64e30b17faf1953cf6131d404
Grzegorz <gregnerga@gmail.com>
2020-09-24 02:29:30 +0200

    init project commit

---
+++ src/main/java/com/nerga/travelCreatorApp/TravelCreatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.nerga.travelCreatorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelCreatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelCreatorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerga/travelCreatorApp/TravelCreatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerga.travelCreatorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelCreatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
