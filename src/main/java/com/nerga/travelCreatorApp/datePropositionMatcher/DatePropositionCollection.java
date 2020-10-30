649bce168b67c85fa73c9f108de5be3503f16925
Grzegorz <grzegorz@pop-os.localdomain>
2020-10-30 22:57:51 +0100

    add datematcher algorithm

---
+++ src/main/java/com/nerga/travelCreatorApp/datePropositionMatcher/DatePropositionCollection.java
@@ -0,0 +1,27 @@
+package com.nerga.travelCreatorApp.datePropositionMatcher;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class DatePropositionCollection {
+
+    List<LocalDate> allDatePropositions;
+    LocalDate min;
+    LocalDate max;
+
+    public DatePropositionCollection (List<LocalDate> allDatePropositions) {
+        this.allDatePropositions = allDatePropositions;
+        this.setMinAndMaxValue();
+    }
+
+    private void setMinAndMaxValue() {
+       this.min = allDatePropositions.stream()
+                .min(LocalDate::compareTo).get();
+        this.max = allDatePropositions.stream()
+                .max(LocalDate::compareTo).get();
+    }
+
+}
