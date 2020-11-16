3dafa749ba12d8e482a6e6cbef051ff5a1fd6778
Grzegorz <gregnerga@gmail.com>
2020-11-16 17:22:24 +0100

    add methods to implement

--- src/main/java/com/nerga/travelCreatorApp/security/jwt/JwtCredentialsAuthenticationRequest.java
+++ src/main/java/com/nerga/travelCreatorApp/security/jwt/JwtCredentialsAuthenticationRequest.java
@@ -1,4 +1,44 @@
 package com.nerga.travelCreatorApp.security.jwt;
 
-public class JwtCredentialsAuthenticationRequest {
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtCredentialsAuthenticationRequest extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtConfig jwtConfig;
+    private final SecretKey secretKey;
+
+    public JwtCredentialsAuthenticationRequest(AuthenticationManager authenticationManager, JwtConfig jwtConfig, SecretKey secretKey) {
+        this.authenticationManager = authenticationManager;
+        this.jwtConfig = jwtConfig;
+        this.secretKey = secretKey;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+
+        return null;
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+
+
+    }
 }
