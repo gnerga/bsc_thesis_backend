4dfe384bb5efcfe3474369405870f192c5a0018a
Grzegorz <gregnerga@gmail.com>
2020-11-19 17:57:42 +0100

    user service clean

--- src/main/java/com/nerga/travelCreatorApp/security/GeneralUserService.java
+++ src/main/java/com/nerga/travelCreatorApp/security/GeneralUserService.java
@@ -32,15 +32,12 @@ public class GeneralUserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-//    public UserEntity createUser(CreateUserDto createUserDto){
-//        return registerUserAccount(createUserDto, UserRole.USER);
-//    }
-
     public Response createUser (CreateUserDto createUserDto){
-        return canRegister(createUserDto).map(user -> registerUserAccount(createUserDto, UserRole.USER)).fold(Function.identity(), Success::accepted);
+        return canRegister(createUserDto)
+                .map(user -> registerUserAccount(createUserDto, UserRole.USER))
+                .fold(Function.identity(), Success::accepted);
     }
 
-//    public Map<String, String> getUserByUsername(String username) {
     public UserIdDto getUserByUsername(String username) {
         UserEntity userEntity = userRepository.findByUsername(username).orElseThrow(() -> new
                 UsernameNotFoundException("PROVIDED_USERNAME_DOSE_NOT_EXIST"));
@@ -53,8 +50,12 @@ public class GeneralUserService {
         return userEntity.toDetailsJson();
     }
 
-    private Optional<UserEntity> findUserByUsername(String username) {
-        return userRepository.findByUsername(username);
+//    private Optional<UserEntity> findUserByUsername(String username) {
+//        return userRepository.findByUsername(username);
+//    }
+
+    private Validation<Error, UserEntity> isUserExist(String username){
+        return null; //todo gnerga implementacja
     }
 
     private UserIdDto registerUserAccount(CreateUserDto createUserDto, UserRole userRole) {
