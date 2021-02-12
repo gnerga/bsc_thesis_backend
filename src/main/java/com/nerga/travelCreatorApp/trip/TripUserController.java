07a32fb4c4223255d5b5b2104b80362d7b51b61e
Grzegorz <gregnerga@gmail.com>
2021-02-12 14:30:02 +0100

    services done

--- src/main/java/com/nerga/travelCreatorApp/trip/TripUserController.java
+++ src/main/java/com/nerga/travelCreatorApp/trip/TripUserController.java
@@ -1,8 +1,11 @@
 package com.nerga.travelCreatorApp.trip;
 
+import com.nerga.travelCreatorApp.datepropositionmatcher.dto.DatePropositionDto;
 import com.nerga.travelCreatorApp.expensesregister.dto.ExpenseRecordCreateDto;
+import com.nerga.travelCreatorApp.expensesregister.dto.ExpenseRecordsUpdateListDto;
 import com.nerga.travelCreatorApp.expensesregister.dto.ExpenseUpdateDto;
 import com.nerga.travelCreatorApp.expensesregister.dto.ExpensesCreateDto;
+import com.nerga.travelCreatorApp.post.dto.PostCreateDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,9 +32,34 @@ public class TripUserController {
         return tripUserService.updateExpense(expenseUpdateDto).toResponseEntity();
     }
 
-    @PostMapping("/addExpenseRecord")
+    @PutMapping("/addExpenseRecord")
     public ResponseEntity addExpenseRecord(@RequestBody ExpenseRecordCreateDto newRecord){
         return tripUserService.addUserToExpense(newRecord).toResponseEntity();
+    }
+
+    @PutMapping("/updateExpenseShareholdersList")
+    public ResponseEntity updateExpenseShareHoldersList(@RequestBody ExpenseRecordsUpdateListDto recordUpdateList){
+        return tripUserService.updateExpenseShareholdersAmount(recordUpdateList).toResponseEntity();
+    }
+
+    @PostMapping("/addPostToTrip={tripId}")
+    public ResponseEntity addPostToTrip(@RequestBody PostCreateDto post, @PathVariable("tripId") Long tripId){
+        return tripUserService.addPostByTripId(post, tripId).toResponseEntity();
+    }
+
+    @PutMapping("/giveHandUpToPost={postId}&{userId}")
+    public ResponseEntity handUp(@PathVariable("postId") Long postId, @PathVariable("userId") Long userId){
+        return tripUserService.handUpByTripAndPostId(postId, userId).toResponseEntity();
+    }
+
+    @PutMapping("/giveHandDownToPost={postId}&{userId}")
+    public ResponseEntity downUp(@PathVariable("postId") Long postId, @PathVariable("userId") Long userId){
+        return tripUserService.handDownByTripAndPostId(postId, userId).toResponseEntity();
+    }
+
+    @PostMapping("/addDatePropositionToTrip={tripId}")
+    public ResponseEntity addDateProposition(@RequestBody DatePropositionDto datePropositionDto, @PathVariable("tripId") Long tripId){
+        return tripUserService.addNewDateProposition(datePropositionDto, tripId).toResponseEntity();
     }
 
 }
