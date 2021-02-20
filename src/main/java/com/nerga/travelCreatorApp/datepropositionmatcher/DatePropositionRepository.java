333d88abb29a6cf27bb14cf39403358c84b7f9e0
Grzegorz <gregnerga@gmail.com>
2021-02-20 17:47:07 +0100

    fix for report

--- src/main/java/com/nerga/travelCreatorApp/datepropositionmatcher/DatePropositionRepository.java
+++ src/main/java/com/nerga/travelCreatorApp/datepropositionmatcher/DatePropositionRepository.java
@@ -1,8 +1,14 @@
 package com.nerga.travelCreatorApp.datepropositionmatcher;
 
+import com.nerga.travelCreatorApp.trip.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DatePropositionRepository extends JpaRepository<DateProposition, Long> {
+
+    List<DateProposition> findAllByOwnerIdAndTrip(Long ownerId, Trip trip);
+
 }
