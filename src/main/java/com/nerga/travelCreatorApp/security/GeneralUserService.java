66fa94b7e0c5db4324d730039ebb64656bff0fae
Grzegorz <gregnerga@gmail.com>
2021-02-21 02:44:57 +0100

    add search;

--- src/main/java/com/nerga/travelCreatorApp/security/GeneralUserService.java
+++ src/main/java/com/nerga/travelCreatorApp/security/GeneralUserService.java
@@ -19,7 +19,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -71,30 +73,33 @@ public class GeneralUserService {
     }
 
     public Response findAllUsersWithoutAuthorizedUserAndContainsGivenText(String text){
-        List<UserDetailsDto> userDetailsDtoList;
-        List<UserDetailsDto> userDetailsDtoList2;
-        List<UserDetailsDto> userDetailsDtoList3;
-                userDetailsDtoList = returnListOfUserDetailsDto(userRepository.findAllByUsernameIsNotAndUsernameContains(
-                        SecurityContextHolder.getContext()
-                                .getAuthentication()
-                                .getPrincipal()
-                                .toString(), text));
-        userDetailsDtoList2 = returnListOfUserDetailsDto(userRepository.findAllByUsernameIsNotAndFirstNameContains(
-                SecurityContextHolder.getContext()
-                        .getAuthentication()
-                        .getPrincipal()
-                        .toString(), text));
-        userDetailsDtoList3 = returnListOfUserDetailsDto(userRepository.findAllByUsernameIsNotAndLastNameContains(
-                SecurityContextHolder.getContext()
-                        .getAuthentication()
-                        .getPrincipal()
-                        .toString(), text));
-        List<UserDetailsDto> temp= Stream.concat(
-                userDetailsDtoList.stream(),
-                userDetailsDtoList2.stream()
-                ).collect(Collectors.toList());
-        List<UserDetailsDto> result = Stream.concat(userDetailsDtoList3.stream(), temp.stream()).collect(Collectors.toList());
-        return !result.isEmpty() ? Success.ok(userDetailsDtoList) : Error.badRequest("USERS_NOT_FOUND");
+
+        List<UserEntity> allUsers = userRepository.findAllByUsernameIsNot( SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal()
+                .toString());
+
+        List<UserEntity> l1 = allUsers.stream()
+                .filter(user-> user.getUsername().toLowerCase(Locale.ROOT)
+                .contains(text.toLowerCase(Locale.ROOT)))
+                .collect(Collectors.toList());
+        List<UserEntity> l2 = allUsers.stream()
+                .filter(user-> user.getFirstName().toLowerCase(Locale.ROOT)
+                        .contains(text.toLowerCase(Locale.ROOT)))
+                .collect(Collectors.toList());
+        List<UserEntity> l3 = allUsers.stream()
+                .filter(user-> user.getLastName().toLowerCase(Locale.ROOT)
+                        .contains(text.toLowerCase(Locale.ROOT)))
+                .collect(Collectors.toList());
+
+        allUsers.clear();
+        allUsers = Stream.of(l1,l2,l3).flatMap(Collection::stream).collect(Collectors.toList());
+
+        List<UserDetailsDto> userDetailsDtoList =
+                returnListOfUserDetailsDto(allUsers);
+
+
+        return !userDetailsDtoList.isEmpty() ? Success.ok(userDetailsDtoList) : Error.badRequest("USERS_NOT_FOUND");
     }
 
     public Response getUserDetailsForLoggedUser(){
